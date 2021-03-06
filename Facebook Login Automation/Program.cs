e516436346364a0f823a8f67af3571cf8e70e241
monalisaulak53 <monalisa01ulak@gmail.com>
2021-03-06 21:51:51 +0545

    Add project files.

---
+++ Facebook Login Automation/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            driver.FindElement(By.Name("email")).SendKeys("test@gmail.com");
+            driver.FindElement(By.Name("pass")).SendKeys("monalisatest");
+            driver.FindElement(By.Name("login")).Click();
+        }
+    }
+}
